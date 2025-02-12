1bf78f03106fa5c4c9d83c92eb5c9bd8d3178b7a
ngjance <63915619+ngjance@users.noreply.github.com>
2025-02-12 16:36:30 +0800

    Update menu.py

--- app/menu.py
+++ app/menu.py
@@ -13,7 +13,7 @@ import json
 
 # Initialize Firebase
 firebase_secrets = st.secrets["firebase"]
-cred = credentials.Certificate("firebase_secrets")
+
 if not firebase_admin._apps:
     cred = credentials.Certificate({
         "type": firebase_secrets["type"],
