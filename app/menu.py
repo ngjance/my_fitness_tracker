667d4f0d0b0b9b7341c633279855fea96a034820
ngjance <63915619+ngjance@users.noreply.github.com>
2025-02-12 16:32:59 +0800

    Update menu.py

--- app/menu.py
+++ app/menu.py
@@ -12,10 +12,22 @@ import time
 import json
 
 # Initialize Firebase
-firebase_secrets = json.loads(st.secrets["firebase"])
+firebase_secrets = st.secrets["firebase"]
 cred = credentials.Certificate("firebase_secrets")
 if not firebase_admin._apps:
-    initialize_app(cred)
+    cred = credentials.Certificate({
+        "type": firebase_secrets["type"],
+        "project_id": firebase_secrets["project_id"],
+        "private_key_id": firebase_secrets["private_key_id"],
+        "private_key": firebase_secrets["private_key"].replace('\\n', '\n'),
+        "client_email": firebase_secrets["client_email"],
+        "client_id": firebase_secrets["client_id"],
+        "auth_uri": firebase_secrets["auth_uri"],
+        "token_uri": firebase_secrets["token_uri"],
+        "auth_provider_x509_cert_url": firebase_secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": firebase_secrets["client_x509_cert_url"]
+    })
+    firebase_admin.initialize_app(cred)
 db = firestore.client()
 storage_client = storage.bucket("fitness-tracker-c51bf.firebasestorage.app")
 
