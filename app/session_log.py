85122caf82b803b52cfedf34668c23a5196bff77
ngjance <63915619+ngjance@users.noreply.github.com>
2025-02-12 16:00:56 +0800

    Update session_log.py

--- app/session_log.py
+++ app/session_log.py
@@ -5,16 +5,17 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
 import firebase_admin
-from firebase_admin import credentials, firestore, storage
+from firebase_admin import credentials, firestore, storage, initialize_app
 from datetime import datetime
 import streamlit_authenticator as stauth
 import bcrypt
 import time
 
 # Initialize Firebase
-cred = credentials.Certificate("your_firebase_service_account.json")
+firebase_secrets = json.loads(st.secrets["firebase"])
+cred = credentials.Certificate("firebase_secrets")
 if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred)
+    initialize_app(cred)
 db = firestore.client()
 storage_client = storage.bucket("fitness-tracker-c51bf.firebasestorage.app")
 
