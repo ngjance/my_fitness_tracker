e36d2904406db30a249cb6b9fff7ffca32b812ad
ngjance <63915619+ngjance@users.noreply.github.com>
2025-03-31 00:51:53 +0800

    Update session_log.py

--- app/session_log.py
+++ app/session_log.py
@@ -76,7 +76,7 @@ else:
             # ------------------- Engineer Data -------------------
 
             # Dropping the rows where reps are time-based
-            session = session[session["rep"].str.contains("s") == False]
+            session = session = session[~session["rep"].astype(str).str.contains("s", na=False)]
             # converting the string to datetime format
             session["sess_date"] = pd.to_datetime(session["sess_date"],format="%d/%m/%Y")
             # Convert "rep" to float64
