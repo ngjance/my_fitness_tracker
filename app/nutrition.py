1ddb12afb06225e0cceeb29f4de709f423dbd647
ngjance <63915619+ngjance@users.noreply.github.com>
2025-06-22 22:24:10 +0800

    Update nutrition.py

--- app/nutrition.py
+++ app/nutrition.py
@@ -104,7 +104,7 @@ else:
                 # Google Sheets credentials and URL setup
                 gs_scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
                 creds_dict = st.secrets["gcp_service_account"]
-                gs_creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+                gs_creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, gs_scope)
                 gc = gspread.authorize(gs_creds)
 
                 sheet_url = "https://docs.google.com/spreadsheets/d/1nfQfBRdZkBcI7ZISrLu1ko-MYmizOTa_EV5panWtJjI"
